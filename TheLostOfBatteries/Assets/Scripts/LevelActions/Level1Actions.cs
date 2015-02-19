8890651be9fe85ea3c25e6a4adbfa51c91c36e9f
marcelo ferrao feodrippe <mff@cin.ufpe.br>
2015-02-19 02:45:00 -0200

    Att
    Level1

---
+++ TheLostOfBatteries/Assets/Scripts/LevelActions/Level1Actions.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class Level1Actions : BaseLevelActions
+{
+    public Player Valdemir;
+    public Player Leo;
+    private CharacterController2D _controllerValdemir;
+    private CharacterController2D _controllerLeo;
+    
+    private volatile bool PunchedLeo;
+    private volatile bool PlayerCollided;
+
+    public void Start()
+    {
+        Valdemir.CanInput = true;
+
+    }
+
+
+    public override void PlayerOnCollider(Collider2D other)
+    {
+       
+    }
+
+    public override void NPCOnCollider(Collider2D other)
+    {
+        
+    }
+
+
+
+}
